2d3e92cb77f329c8f66f0d4cfdc867a0438219c3
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-10-14 22:10:24 +0700

    changes: permanent to false

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    return [{ source: "/", destination: "/pilih-gender", permanent: true }];
+    return [{ source: "/", destination: "/pilih-gender", permanent: false }];
   },
   images: {
     domains: ["res.cloudinary.com"],
