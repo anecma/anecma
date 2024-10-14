2e3b14d96ddfe5633e03ae4d64836ccc9d83cbac
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-10-14 20:55:13 +0700

    changes: update initial redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com'],
-    },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  async redirect() {
+    return [{ source: "/", destination: "/pilih-gender", permanent: false }];
+  },
 };
 
 export default nextConfig;
