5a4d96723b633d25691ec5e04d98f9d38286da6b
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-10-14 22:09:43 +0700

    changes: google

--- src/components/button/google-login-button.tsx
+++ src/components/button/google-login-button.tsx
@@ -10,7 +10,7 @@ export default function GoogleLoginButton() {
       className="flex flex-row gap-4 items-center justify-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-4 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 min-w-full"
       onClick={() => {
         signIn("google", {
-          callbackUrl: "https://anecma.id/istri/dashboard",
+          callbackUrl: "https://www.anecma.id/istri/dashboard",
         });
       }}
     >
