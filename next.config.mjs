8438da6c24870ded2a2c92a2700ba610caf9ccbc
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2024-10-14 21:12:45 +0700

    changes: fixing typos

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirect() {
+  async redirects() {
     return [{ source: "/", destination: "/pilih-gender", permanent: true }];
   },
   images: {
