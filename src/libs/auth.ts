7cc750ca87176e084125848e62611c630e4aa671
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-04-03 21:32:08 +0700

    changes: update strategy

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -9,6 +9,9 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
   trustHost: true,
   secret: process.env.NEXTAUTH_SECRET!,
   callbacks: {
